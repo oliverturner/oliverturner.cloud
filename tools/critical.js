61b41e49d0a60050152e1a2b708d2dc069e71381
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-03-09 13:25:35 +0000

    Fix `critical` config

--- tools/critical.js
+++ tools/critical.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const critical = require("critical");
 
-critical.generate({
-  inline: true,
-  minify: true,
-  base: "./_site",
-  src: "index.html",
-  dest: "index.html"
-});
+critical.generate(
+  {
+    inline: true,
+    minify: true,
+    base: "./_site",
+    src: "index.html",
+    target: "index.html",
+  },
+  (err) => {
+    console.log({ err });
+  }
+);
